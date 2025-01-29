ec2835fdd44187b866ac28c619e20f990a2ddc9f
Sassan Haradji <me@sassanh.com>
2025-01-29 14:35:43 +0400

    feat(web-ui): add `ubo-redirect-server` service

--- ubo_app/system/redirect_server.py
+++ ubo_app/system/redirect_server.py
@@ -26,16 +26,9 @@ REDIRECT_PATHS = [
     '/connecttest.txt',  # Windows 11 variant
 ]
 
-shutdown_event: asyncio.Event | None = None
-
 
 async def start_redirect_server() -> None:
     """Start the HTTP server that will redirect all requests to the web UI."""
-    global shutdown_event  # noqa: PLW0603
-    if shutdown_event:
-        return
-    shutdown_event = asyncio.Event()
-
     app = Quart('ubo-app-redirect-server')
     app.debug = WEB_UI_DEBUG_MODE
 
@@ -51,21 +44,13 @@ async def start_redirect_server() -> None:
 
         return '', 302, {'Location': f'http://{CAPTIVE_IP}:{PORT_HTTP}/'}
 
-    async def wait_for_shutdown() -> None:
-        if shutdown_event:
-            await shutdown_event.wait()
-
     await app.run_task(
         host='0.0.0.0',  # noqa: S104
         port=80,
         debug=WEB_UI_DEBUG_MODE,
-        shutdown_trigger=wait_for_shutdown,
     )
 
 
-def stop_redirect_server() -> None:
-    """Stop the HTTP server."""
-    global shutdown_event  # noqa: PLW0603
-    if shutdown_event:
-        shutdown_event.set()
-        shutdown_event = None
+def main() -> None:
+    """Start the HTTP server."""
+    asyncio.run(start_redirect_server())
--- ubo_app/system/system_manager/hotspot.py
+++ ubo_app/system/system_manager/hotspot.py
@@ -1,18 +1,17 @@
 """Set up a hotspot on the UBO."""
 
-import asyncio
 import pathlib
 import subprocess
 
-from ubo_app.system.system_manager.hotspot_server import (
-    start_redirect_server,
-    stop_redirect_server,
-)
+from ubo_app.logger import get_logger
 from ubo_app.utils.pod_id import get_pod_id
 from ubo_app.utils.template_files import copy_templates, restore_backups
 
+logger = get_logger('system-manager')
 
-def _start() -> None:
+
+def _start() -> str:
+    logger.info('Starting the hotspot')
     templates_path = pathlib.Path(__file__).parent / 'hotspot_templates'
     copy_templates(
         templates_path,
@@ -42,28 +41,47 @@ def _start() -> None:
         )
         subprocess.run(  # noqa: S603
             ['/usr/bin/env', 'systemctl', 'start', 'hostapd'],
-            check=False,
+            check=True,
+        )
+        subprocess.run(  # noqa: S603
+            ['/usr/bin/env', 'systemctl', 'start', 'ubo-redirect-server'],
+            check=True,
         )
     except subprocess.CalledProcessError:
+        logger.exception('Failed to start the hotspot properly, stopping it...')
         _stop()
+        return 'failed'
     else:
-        asyncio.run(start_redirect_server())
+        logger.info('Hotspot started')
+        return 'done'
 
 
-def _stop() -> None:
-    stop_redirect_server()
+def _stop() -> str:
+    logger.info('Stopping the hotspot')
     templates_path = pathlib.Path(__file__).parent / 'hotspot_templates'
     restore_backups(templates_path)
-    subprocess.run(['/usr/bin/env', 'systemctl', 'stop', 'hostapd'], check=False)  # noqa: S603
-    subprocess.run(['/usr/bin/env', 'systemctl', 'disable', 'hostapd'], check=False)  # noqa: S603
-    subprocess.run(['/usr/bin/env', 'systemctl', 'mask', 'hostapd'], check=True)  # noqa: S603
-    subprocess.run(['/usr/bin/env', 'systemctl', 'stop', 'dnsmasq'], check=False)  # noqa: S603
-    subprocess.run(['/usr/bin/env', 'systemctl', 'restart', 'dhcpcd'], check=False)  # noqa: S603
+    try:
+        subprocess.run(  # noqa: S603
+            ['/usr/bin/env', 'systemctl', 'stop', 'ubo-redirect-server'],
+            check=False,
+        )
+        subprocess.run(['/usr/bin/env', 'systemctl', 'stop', 'hostapd'], check=False)  # noqa: S603
+        subprocess.run(['/usr/bin/env', 'systemctl', 'disable', 'hostapd'], check=False)  # noqa: S603
+        subprocess.run(['/usr/bin/env', 'systemctl', 'mask', 'hostapd'], check=False)  # noqa: S603
+        subprocess.run(['/usr/bin/env', 'systemctl', 'stop', 'dnsmasq'], check=False)  # noqa: S603
+        subprocess.run(['/usr/bin/env', 'systemctl', 'restart', 'dhcpcd'], check=False)  # noqa: S603
+    except subprocess.CalledProcessError:
+        logger.exception('Failed to stop the hotspot properly')
+        return 'failed'
+    else:
+        logger.info('Hotspot stopped')
+        return 'done'
 
 
-def hotspot_handler(desired_state: str) -> None:
+def hotspot_handler(desired_state: str) -> str:
     """Set up a hotspot on the UBO."""
     if desired_state == 'start':
-        _start()
-    elif desired_state == 'stop':
-        _stop()
+        return _start()
+    if desired_state == 'stop':
+        return _stop()
+    return 'unknown command'
