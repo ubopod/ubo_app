a91e29029d3ad0086ed73a03eb0a5ff3afb21b83
Sassan Haradji <me@sassanh.com>
2025-03-01 05:12:16 +0400

    chore(web-ui): add linting and formatting to the web application code - closes #224

---
+++ ubo_app/services/090-web-ui/web-app/eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginImport from "eslint-plugin-import";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: { import: pluginImport },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+
+      "import/no-duplicates": "error",
+
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+];
