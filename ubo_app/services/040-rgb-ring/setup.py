94bd61bf1ea82f947d5c9e9021d3b33932063c4b
Sassan Haradji <me@sassanh.com>
2025-03-08 09:45:32 +0400

    refactor(core,audio,display,keypad,sensors): read and check content of eeprom to determine if a device/service should be initialized or not - closes #223, #closes #249

--- ubo_app/services/040-rgb-ring/setup.py
+++ ubo_app/services/040-rgb-ring/setup.py
@@ -1,9 +1,20 @@
 # ruff: noqa: D100, D101, D102, D103, D104, D107, N999
 from ubo_app.store.main import store
 from ubo_app.store.services.rgb_ring import RgbRingCommandEvent, RgbRingPulseAction
+from ubo_app.utils.eeprom import get_eeprom_data
 
 
 def init_service() -> None:
+    eeprom_data = get_eeprom_data()
+
+    if (
+        eeprom_data is None
+        or 'led' not in eeprom_data
+        or eeprom_data['led'] is None
+        or eeprom_data['led']['model'] != 'neopixel'
+    ):
+        return
+
     from rgb_ring_client import RgbRingClient
 
     rgb_ring_client = RgbRingClient()
