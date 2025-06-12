27fcb0e44c8a678f3cabd9f13ac1635be176c642
Sassan Haradji <me@sassanh.com>
2025-06-12 04:40:15 +0400

    refactor(core:rpc): rename `generated` to `ubo_bindings`, we want to reuse that directory to create a separate python package for the rpc bindings, so that other services can use it without depending on the whole `ubo_app` package

---
+++ ubo_app/rpc/client.py
@@ -0,0 +1,55 @@
+"""Async remote store client for dispatching operations to a gRPC server."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, overload
+
+from grpclib.client import Channel
+
+from ubo_bindings.store.v1 import (
+    DispatchActionRequest,
+    DispatchEventRequest,
+    StoreServiceStub,
+    SubscribeEventRequest,
+)
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from ubo_bindings.ubo.v1 import Action, Event
+
+
+class AsyncRemoteStore:
+    """Async remote store for dispatching operations to a gRPC server."""
+
+    def __init__(self, host: str, port: int) -> None:
+        """Initialize the async remote store."""
+        self.channel = Channel(host=host, port=port)
+        self.service = StoreServiceStub(self.channel)
+
+    @overload
+    async def dispatch_async(self, *, action: Action) -> None: ...
+    @overload
+    async def dispatch_async(self, *, event: Event) -> None: ...
+    async def dispatch_async(
+        self,
+        *,
+        action: Action | None = None,
+        event: Event | None = None,
+    ) -> None:
+        """Dispatch an operation to the remote store."""
+        if action is not None:
+            await self.service.dispatch_action(DispatchActionRequest(action=action))
+        if event is not None:
+            await self.service.dispatch_event(DispatchEventRequest(event=event))
+
+    async def subscribe_event(
+        self,
+        event_type: Event,
+        callback: Callable[[Event], None],
+    ) -> None:
+        """Subscribe to the remote store."""
+        async for response in self.service.subscribe_event(
+            SubscribeEventRequest(event=event_type),
+        ):
+            callback(response.event)
--- ubo_app/rpc/service.py
+++ ubo_app/rpc/service.py
@@ -6,18 +6,17 @@ from asyncio import Queue, QueueFull
 from typing import TYPE_CHECKING, Any, cast
 
 import betterproto
-
 from ubo_app.logger import logger
-from ubo_app.rpc.generated.store.v1 import (
+from ubo_app.rpc.message_to_object import get_class, rebuild_object, reduce_group
+from ubo_app.rpc.object_to_message import build_message
+from ubo_app.rpc.ubo_bindings.store.v1 import (
     DispatchActionRequest,
     DispatchActionResponse,
     StoreServiceBase,
     SubscribeEventRequest,
     SubscribeEventResponse,
 )
-from ubo_app.rpc.generated.ubo.v1 import Event
-from ubo_app.rpc.message_to_object import get_class, rebuild_object, reduce_group
-from ubo_app.rpc.object_to_message import build_message
+from ubo_app.rpc.ubo_bindings.ubo.v1 import Event
 from ubo_app.store.main import UboAction, UboEvent, store
 
 if TYPE_CHECKING:
@@ -38,20 +37,20 @@ class StoreService(StoreServiceBase):
             action = rebuild_object(dispatch_action_request.action)
         except Exception:
             logger.exception(
-                'Failed to build object from dispatch action request coming from gRPC',
+                "Failed to build object from dispatch action request coming from gRPC",
                 extra={
-                    'request': dispatch_action_request,
+                    "request": dispatch_action_request,
                 },
             )
         else:
             logger.info(
-                'Dispatching action coming from gRPC',
+                "Dispatching action coming from gRPC",
                 extra={
-                    'request': dispatch_action_request,
-                    'action': action,
+                    "request": dispatch_action_request,
+                    "action": action,
                 },
             )
-            store.dispatch(cast('UboAction', action))
+            store.dispatch(cast("UboAction", action))
         return DispatchActionResponse()
 
     async def subscribe_event(
@@ -60,8 +59,8 @@ class StoreService(StoreServiceBase):
     ) -> AsyncIterator[SubscribeEventResponse]:
         """Subscribe to an event from the store."""
         logger.info(
-            'Received event subscription over gRPC',
-            extra={'request': subscribe_event_request},
+            "Received event subscription over gRPC",
+            extra={"request": subscribe_event_request},
         )
         event_class = get_class(reduce_group(subscribe_event_request.event))
         queue: Queue[UboEvent] = Queue(30)
@@ -73,10 +72,10 @@ class StoreService(StoreServiceBase):
                     queue.put_nowait(event)
                 except QueueFull:
                     logger.verbose(
-                        'Subscription event queue is full, dropping event',
+                        "Subscription event queue is full, dropping event",
                         extra={
-                            'event': event,
-                            'queue_size': queue.qsize(),
+                            "event": event,
+                            "queue_size": queue.qsize(),
                         },
                     )
 
@@ -88,7 +87,7 @@ class StoreService(StoreServiceBase):
                         **{
                             betterproto.casing.snake_case(
                                 type(event).__name__,
-                            ): cast('Any', build_message(event)),
+                            ): cast("Any", build_message(event)),
                         },
                     ),
                 )
