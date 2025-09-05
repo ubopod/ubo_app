fed71e3799828b846150b28edacecac53d0ca4ff
Sassan Haradji <me@sassanh.com>
2025-09-05 04:34:31 +0400

    feat(assistant): the assistant can now see the screen content of the pod's display on user demand

--- ubo_app/services/090-assistant/ubo-service/ubo_assistant/image_frame.py
+++ ubo_app/services/090-assistant/ubo-service/ubo_assistant/image_frame.py
@@ -1,7 +1,10 @@
 """Input frame for image generation services."""
 
+from dataclasses import dataclass
+
 from pipecat.frames.frames import TextFrame
 
 
+@dataclass
 class ImageGenFrame(TextFrame):
     """Frame to be consumed by image generation services."""
