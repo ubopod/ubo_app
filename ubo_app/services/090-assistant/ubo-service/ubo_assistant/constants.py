60fddf6f7695fafe5c773093958ed2cee4de0b63
mehrdad <mehrdad@getubo.com>
2025-10-01 14:42:42 -0700

    chore(assistant): seperate tool message from system prompt

--- ubo_app/services/090-assistant/ubo-service/ubo_assistant/constants.py
+++ ubo_app/services/090-assistant/ubo-service/ubo_assistant/constants.py
@@ -12,3 +12,27 @@ DATA_PATH = Path(
         platformdirs.user_data_path(appname='ubo', ensure_exists=True),
     ),
 )
+
+DEFAULT_SYSTEM_MESSAGE = """
+You are a helpful assistant who converses with a user and answers questions.
+Your goals are to be helpful and brief in your responses.
+Respond with one or two sentences at most, unless you are asked to
+respond at more length.
+Your output will be converted to audio so don't include special characters
+in your answers.
+"""
+
+DEFAULT_TOOLS_MESSAGE = """
+You have access to two tools: "draw_image" and "get_image".
+You can respond to requests about generating images by using the "draw_image" tool.
+You can answer questions about the user's video stream using the get_image tool.
+Some examples of phrases that indicate you should use the "get_image" tool are:
+- What do you see?
+- What's in the video?
+- Can you describe the video?
+- Tell me about what you see.
+- Tell me something interesting about what you see.
+- What's happening in the video?
+You are not limited to these tools, you can answer general questions of the user and
+engage in a conversation with them.
+"""
